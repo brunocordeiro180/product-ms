f08a5ea42d546b7d337f4964cae3537ed88592bc
Bruno Cordeiro <brunocordeiro180@gmail.com>
2021-06-14 01:31:25 -0300

    Add controller documentation

--- src/main/java/uol/compasso/productms/controller/ProductController.java
+++ src/main/java/uol/compasso/productms/controller/ProductController.java
@@ -23,6 +23,11 @@ public class ProductController {
 
     private final ProductService productService;
 
+    /**
+     * Find all products from db.
+     *
+     * @return List of products
+     */
     @GetMapping
     public ResponseEntity<List<Product>> getProducts(){
         log.info("ProductController: getProducts");
@@ -30,6 +35,12 @@ public class ProductController {
         return ResponseEntity.ok(products);
     }
 
+    /**
+     * Find product by id
+     *
+     * @param productId Product id
+     * @return Product found or Error Message
+     */
     @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> getProductById(@PathVariable("id") String productId){
         log.info("ProductController: getProductById");
@@ -42,6 +53,11 @@ public class ProductController {
         }
     }
 
+    /**
+     * Create a new product
+     *
+     * @return Product created
+     */
     @PostMapping
     public ResponseEntity<Product> createProduct(@RequestBody ProductDto productDto){
         log.info("ProductController: createProduct -> " + productDto);
@@ -50,6 +66,12 @@ public class ProductController {
         return ResponseEntity.ok(newProduct);
     }
 
+    /**
+     * Delete product by id
+     *
+     * @param productId Product id
+     * @return Status code 200 if deleted or 404 if product not found
+     */
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<HttpStatus> deleteProduct(@PathVariable("id") String productId){
         log.info("ProductController: deleteProduct -> id: " + productId);
@@ -61,6 +83,13 @@ public class ProductController {
         }
     }
 
+    /**
+     * Update product by id
+     *
+     * @param productId Product id
+     * @param productDto New attributes for the product
+     * @return Product updated or Status Code 404 if product not found
+     */
     @PutMapping(path = "/{id}")
     public ResponseEntity<Object> updateProduct(@PathVariable("id") String productId, @RequestBody ProductDto productDto){
         log.info("ProductController: updateProduct -> id: " + productId + " , productDto: " + productDto);
@@ -73,6 +102,14 @@ public class ProductController {
         }
     }
 
+    /**
+     * Search product from query url
+     *
+     * @param minPrice Minimum price of the product
+     * @param maxPrice Maximium price of the product
+     * @param q Product name or description
+     * @return List of products found from query string
+     */
     @GetMapping(path = "/search")
     public ResponseEntity<List<Product>> searchProducts(
             @RequestParam(value = "min_price" , required = false) BigDecimal minPrice,

4c8a11509c07bd208457716ef921519b7690fa51
Bruno Cordeiro <brunocordeiro180@gmail.com>
2021-06-14 01:15:34 -0300

    Adding README.md

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,8 +6,3 @@ CREATE TABLE product(
     description VARCHAR(500) NOT NULL,
     price DECIMAL(10,2) NOT NULL
 );
-
-INSERT INTO product(name, description, price) VALUES
-('product1', 'First Product', 10.50),
-('product2', 'Second Product', 5.60),
-('product3', 'Third Product', 2.30);
